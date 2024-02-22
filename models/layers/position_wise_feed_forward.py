bb7ba2280dca95db827e997be629701029a8d336
Messiah <messiah@DESKTOP-UM9JRMJ.localdomain>
2024-02-22 04:16:23 -0500

    add layers

--- models/layers/position_wise_feed_forward.py
+++ models/layers/position_wise_feed_forward.py
@@ -1,9 +1,9 @@
 from torch import nn
 
-class PosWiseFF(nn.Module):
+class PositionwiseFeedForward(nn.Module):
 
     def __init__(self, d_model, hidden, dropout_prob=0.1):
-        super(PosWiseFF, self).__init__()
+        super(PositionwiseFeedForward, self).__init__()
         self.linear_1 = nn.Linear(d_model, hidden)
         self.linear_2 = nn.Linear(hidden, d_model)
         self.relu = nn.ReLU()
