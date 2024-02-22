73f4144ec65a0337f4c8e40c5a6fdecf0dbf4fd2
Messiah <messiah@DESKTOP-UM9JRMJ.localdomain>
2024-02-22 03:10:58 -0500

    add folders and pos_wise_ff.py

---
+++ models/layers/pos_wise_ff.py
@@ -0,0 +1,20 @@
+from torch import nn
+
+class PosWiseFF(nn.Module):
+
+    def __init__(self, d_model, hidden, dropout_prob=0.1):
+        super(PosWiseFF, self).__init__()
+        self.linear_1 = nn.Linear(d_model, hidden)
+        self.linear_2 = nn.Linear(hidden, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=dropout_prob)
+
+    def forward(self, x):
+        x = self.linear_1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear_2(x)
+        return x
+
+# if __name__ == '__main__':
+#     return
